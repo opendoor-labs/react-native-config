92f39238af25e727fae0c7f154aa4278b7e31d90
Zain Memon <zain@inzain.net>
2017-09-20 20:58:59 -0700

    Fix compatibility issue w/ react native 0.48
    see https://github.com/yonahforst/react-native-permissions/pull/138 for another example of this fix

--- ios/ReactNativeConfig/ReactNativeConfig.h
+++ ios/ReactNativeConfig/ReactNativeConfig.h
@@ -1,4 +1,9 @@
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
+
 
 @interface ReactNativeConfig : NSObject <RCTBridgeModule>
 
